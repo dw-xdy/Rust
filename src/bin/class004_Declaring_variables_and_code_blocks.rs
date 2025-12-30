43d46c8f5af3998b096b6bc01b52734ac183c151
dw-xdy <3115708251@qq.com>
2025-12-30 22:00:07 +0800

    了解了关于代码块和print展示的问题

---
+++ src/bin/class004_Declaring_variables_and_code_blocks.rs
@@ -0,0 +1,21 @@
+// fn main() {
+//     {
+//         let my_number = 8; // my_number starts here
+//         // my_number ends here!
+//     }
+//
+//     println!("Hello, number {}", my_number); // ⚠️ there is no my_number and
+//     // println!() can't find it
+// }
+
+
+fn main() {
+
+    let my_number = {
+        let second_number = 8;
+        second_number + 9 // No semicolon, so the code block returns 8 + 9.
+        // It works just like a function
+    };
+
+    println!("My number is: {}", my_number);
+}
