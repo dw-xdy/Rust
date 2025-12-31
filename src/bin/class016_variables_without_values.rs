1f854aa955e1d010709572736b2b8639edb8a759
dw-xdy <3115708251@qq.com>
2025-12-31 22:46:47 +0800

    学习了关于Rust中的未赋初值的变量.

---
+++ src/bin/class016_variables_without_values.rs
@@ -0,0 +1,26 @@
+fn loop_then_return(mut counter: i32) -> i32 {
+    loop {
+        counter += 1;
+        if counter % 50 == 0 {
+            break;
+        }
+    }
+    counter
+}
+
+fn main() {
+    let my_number;
+
+    {
+        // Pretend we need to have this code block
+        let number = {
+            // Pretend there is code here to make a number
+            // Lots of code, and finally:
+            57
+        };
+
+        my_number = loop_then_return(number);
+    }
+
+    println!("{}", my_number);
+}
