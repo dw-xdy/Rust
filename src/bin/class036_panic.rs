10fc51bb6796665a97216570cce6584ab25bc666
dw-xdy <3115708251@qq.com>
2026-01-04 22:56:54 +0800

    学习了Rust中的panic!

---
+++ src/bin/class036_panic.rs
@@ -0,0 +1,15 @@
+// fn main() {
+//     panic!("Time to panic!");
+// }
+
+fn prints_three_things(vector: Vec<i32>) {
+    if vector.len() != 3 {
+        panic!("my_vec must always have three items") // will panic if the length is not 3
+    }
+    println!("{}, {}, {}", vector[0], vector[1], vector[2]);
+}
+
+fn main() {
+    let my_vec = vec![8, 9, 10];
+    prints_three_things(my_vec);
+}
