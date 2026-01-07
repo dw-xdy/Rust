a52a59ad17d9c0facad986957a6da847b2b711bb
dw-xdy <3115708251@qq.com>
2026-01-07 23:24:04 +0800

    学习了Rust中的dbg!()操作, 总体来说还是比较简单的, 而且也学习了inspect()这个方便调试的方法.

---
+++ src/bin/class042_dbg.rs
@@ -0,0 +1,34 @@
+// fn main() {
+//     let number = 9;
+//     dbg!(number);
+// }
+
+// fn main() {
+//     let mut my_number = dbg!(9);
+//     dbg!(my_number += 10);
+//
+//     let new_vec = dbg!(vec![8, 9, 10]);
+//
+//     let double_vec = dbg!(new_vec.iter().map(|x| x * 2).collect::<Vec<i32>>());
+//
+//     dbg!(double_vec);
+// }
+
+
+fn main() {
+    let new_vec = vec![8, 9, 10];
+
+    let double_vec = new_vec
+        .iter()
+        .inspect(|first_item| {
+            println!("The item is: {first_item}");
+            match **first_item % 2 { // first item is a &&i32 so we use **
+                0 => println!("It is even."),
+                _ => println!("It is odd."),
+            }
+            println!("In binary it is {:b}.", first_item);
+        })
+        .map(|x| x * 2)
+        .collect::<Vec<i32>>();
+}
+
