11b10bb88ff421d4058bd49255628644190a4624
dw-xdy <3115708251@qq.com>
2026-01-12 23:31:31 +0800

    Rust中的Mutex第一部分完成

---
+++ src/bin/class047_Mutex.rs
@@ -0,0 +1,33 @@
+// Mutex : mutual exclusion.
+
+use std::sync::Mutex;
+
+fn main() {
+    let my_mutex = Mutex::new(5); // A new Mutex<i32>. We don't need to say mut
+    {
+        // we can use it to auto drop
+        let mut mutex_changer = my_mutex.lock().unwrap(); // mutex_changer is a MutexGuard
+        // It has to be mut because we will change it
+        // Now it has access to the Mutex
+        // Let's print my_mutex to see:
+
+        println!("{:?}", my_mutex); // This prints "Mutex { data: <locked> }"
+        // So we can't access the data with my_mutex now,
+        // only with mutex_changer
+
+        println!("{:?}", mutex_changer); // This prints 5. Let's change it to 6.
+
+        *mutex_changer = 6; // mutex_changer is a MutexGuard<i32> so we use * to change the i32
+
+        println!("{:?}", mutex_changer); // Now it says 6
+    }
+
+    // or we can use std::mem::drop(mutex_changer)
+
+    // if mutex_change variable not be dropped, we can't touch it again. (Deadlock)
+    let mut mutex_changer2 = my_mutex.lock().unwrap();
+    println!("{:?}", my_mutex);
+    println!("{:?}", mutex_changer2);
+    *mutex_changer2 = 213;
+    println!("{:?}", mutex_changer2);
+}
