2dfec4b792cd72684f0cee24c8f0e32f13c71e90
dw-xdy <3115708251@qq.com>
2026-03-12 18:23:31 +0800

    重新复习了一次vec

--- src/bin/class018_vectors.rs
+++ src/bin/class018_vectors.rs
@@ -1,3 +1,5 @@
+use std::string;
+
 // fn main() {
 //     let vec_of_ten = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 //     // Everything is the same as above except we added vec!.
@@ -23,8 +25,10 @@
 fn main() {
     let mut num_vec = Vec::new();
     // that also 2 * capacity to increase.
+    // 可以使用 with_capacity() 方法来进行指定初始容量的大小。
     // let mut num_vec: Vec<char> = Vec::with_capacity(5);
 
+    // 这个和Java中的ArrayList一样，都是按照相同的方式进行增长的。二倍关系。
     println!("{}", num_vec.capacity()); // 0 elements: prints 0
     num_vec.push('a'); // add one character
     println!("{}", num_vec.capacity()); // 1 element: prints 4. Vecs with 1 item always start with capacity 4
@@ -41,12 +45,51 @@ fn main() {
     println!("{}", num_vec.capacity()); // prints 16. We have 9 elements, but it doubled  8 to 16 to make space
 
     // must be &str, not: String.
-    let my_vec: Vec<&str> = ["kfdsa", "kfdsahlk", "giodsana"].into();
-    println!("{:?}", my_vec);
+    let my_vec: Vec<String> = [
+        "kfdsa".to_string(),
+        "kfdsahlk".to_string(),
+        "giodsana".to_string(),
+    ]
+    .into();
+    println!("{:#?}", my_vec);
 
     let my_vec: Vec<u8> = [1, 2, 3].into();
     let my_vec2: Vec<_> = [9, 0, 10].into(); // Vec<_> means "choose the Vec type for me"
     // Rust will choose Vec<i32>
     let my_vec2: Vec<_> = ["first", "second", "third"].into(); // Vec<_> means "choose the Vec type for me"
     // Rust will choose Vec<&str>
+
+    // 使用该方法指定初始值的时候，依旧会按照两倍的大小重新扩容，对应的内存位置也会发生改变。
+    let mut my_vec3: Vec<u8> = Vec::with_capacity(5);
+
+    println!("{:#?}", my_vec3.capacity());
+
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    my_vec3.push(3);
+    println!("{:#?}", my_vec3.capacity());
 }
